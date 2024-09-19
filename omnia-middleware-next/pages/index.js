13f987be5c1c4e4e7d5dee7341991077d5f676cb
AndiappanAR <andiappan.ravi@omnia.ae>
2024-09-19 11:11:12 +0400

    Label changes

--- omnia-middleware-next/pages/index.js
+++ omnia-middleware-next/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">OMNIA middleware service!</a>
+          Welcome to <a href="https://nextjs.org">OMNIA micro service!</a>
         </h1>
 
         <div className={styles.grid}>
